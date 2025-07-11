aa65a2317a00a64eddd4deeb5472643a535ee10b
0xFelipe <felipe-souza-mendes@outlook.com>
2025-07-11 09:08:16 -0300

    Projeto calculadora de IMC

---
+++ CalculadoradeIMC/app/src/main/java/com/fsmtech/calculadoradeimc/MainActivity.java
@@ -0,0 +1,95 @@
+package com.fsmtech.calculadoradeimc;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.fsmtech.calculadoradeimc.databinding.ActivityMainBinding;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.btCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String peso = binding.editPeso.getText().toString();
+                String altura = binding.editAltura.getText().toString();
+
+                if(peso.isEmpty()){
+                    binding.editPeso.setError("Informe seu Peso!");
+                }else if (altura.isEmpty()){
+                    binding.editAltura.setError("Informe sua Altura!");
+                }else {
+                    calcularImc();
+                }
+            }
+        });
+    }
+
+    private void calcularImc(){
+
+        float peso = Float.parseFloat(binding.editPeso.getText().toString().replace(",","."));
+        float altura = Float.parseFloat(binding.editAltura.getText().toString().replace(",","."));
+        float imc = peso / (altura * altura);
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");decimalFormat.format(imc);
+
+        if (imc < 18.5){
+            binding.txtResultado.setText("Seu IMC é de " + decimalFormat.format(imc) + "\n" + "Abaixo do Peso");
+        }else if (imc <= 24.9){
+            binding.txtResultado.setText("Seu IMC é de " + decimalFormat.format(imc) + "\n" + "Peso normal");
+        }else if (imc <= 29.9){
+            binding.txtResultado.setText("Seu IMC é de " + decimalFormat.format(imc) + "\n" + "Sobrepeso");
+        }else if (imc <= 34.9){
+            binding.txtResultado.setText("Seu IMC é de " + decimalFormat.format(imc) + "\n" + "Obesidade (grau 1)");
+        }else if (imc <= 39.9){
+            binding.txtResultado.setText("Seu IMC é de " + decimalFormat.format(imc) + "\n" + "Obesidade Severa (grau 2)");
+        }else{
+            binding.txtResultado.setText("Seu IMC é de " + decimalFormat.format(imc) + "\n" + "Obesidade Mórbida (grau 3)");
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+
+        if (itemID == R.id.cleaning){
+            binding.editPeso.setText("");
+            binding.editAltura.setText("");
+            binding.txtResultado.setText("");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
